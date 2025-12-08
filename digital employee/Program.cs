073b535ee1aabfaebc51ec71479a182f54adf9ef
Maielshazlyy <maielshazly74@icloud.com>
2025-12-08 23:29:37 +0200

    register fluent validator

--- digital employee/Program.cs
+++ digital employee/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using Service_layer;
 using DAL.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace digital_employee
 {
@@ -9,28 +12,69 @@ namespace digital_employee
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            // -------------------------
+            // 1) Controllers
+            // -------------------------
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-            builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            // -------------------------
+            // 2) OpenAPI / Swagger
+            // -------------------------
+            builder.Services.AddOpenApi();
+
+            // -------------------------
+            // 3) Database Connection
+            // -------------------------
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // -------------------------
+            // 4) FluentValidation Registration
+            // -------------------------
+            builder.Services.AddValidatorsFromAssembly(typeof(AssemblyReference).Assembly);
+
+            // -------------------------
+            // 5) Custom Validation Response
+            // -------------------------
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = context =>
+                {
+                    var errors = context.ModelState
+                        .Where(e => e.Value.Errors.Count > 0)
+                        .Select(e => new
+                        {
+                            Field = e.Key,
+                            Error = e.Value.Errors.First().ErrorMessage
+                        });
+
+                    return new BadRequestObjectResult(new
+                    {
+                        Message = "Validation Failed",
+                        Errors = errors
+                    });
+                };
+            });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // -------------------------
+            // 6) Swagger in Development
+            // -------------------------
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
 
+            // -------------------------
+            // 7) Middlewares
+            // -------------------------
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
-
+            // -------------------------
+            // 8) Controllers Endpoint
+            // -------------------------
             app.MapControllers();
 
             app.Run();
